18bb89daec5290d7f39b67bfe04bc5fa065d2b23
sai <saikumar.repalle@rackspace.com>
2021-01-19 14:47:49 +0530

    master001

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,8 @@
 # Get the private key from the environment variable
 echo "Setting up Production Connection..."
 mkdir keys
-echo $SFDC_SERVER_KEY | "-----BEGINRSAPRIVATEKEY-----" + base64 -d + "-----ENDRSAPRIVATEKEY-----"> keys/server.key
+echo $SFDC_SERVER_KEY > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile $SFDC_SERVER_KEY --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
